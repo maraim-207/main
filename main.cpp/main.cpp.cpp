7f8acc7458b16c1158b0d6f88207f47eb3ade7e9
maraim-207 <maraimyasser23@gmail.com>
2026-04-07 13:01:47 +0200

    Refactor mood entry input and remove duplicate variable
    Removed redundant moodCount variable and added input prompts for mood type and level.

--- main.cpp/main.cpp.cpp
+++ main.cpp/main.cpp.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int count = 0;
+int moodCount = 0;
 struct moodEntry
 {
 	int day;
@@ -19,21 +19,19 @@ void addMood(moodEntry moods[])
 	cin >> moods->month;
 	cout << "Enter the year\n";
 	cin >> moods->year;
+	cout << "Enter the mood type \n";
+	cout << "Happy\tSad\tAngry\tStressed\tcalm\n";
+	cin >> moods->moodtype;
 	cout << "Enter the mood level\n";
 	cin >> moods->moodLevel;
 	do
 	{
 		cout << "wrong input please enter a number between 1 and 5\n";
 	} while (moods->moodLevel > 5 || moods->moodLevel < 1) ;
-	cout << "Enter the mood type \n";
-	cin >> moods->moodtype;
 	cout << "Enter the note\n";
 	cin >> moods->note;
 }
 int main()
 {
-
-	int moodCount = 0;
-	 
 	return 0;
 }
