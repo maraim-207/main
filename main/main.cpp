84e7222efa30fa2530801bb5163d42a57f82d045
mariam <maraimyasser23@gmail.com>
2026-04-07 11:48:59 +0200

    test 1

--- main/main.cpp
+++ main/main.cpp
@@ -1,46 +1,39 @@
-#include <iostream>
-#include <numeric> // For std::accumulate (optional, but a good practice)
-
-// Function to calculate factorial of a number
-long long calculateFactorial(int n) {
-    if (n < 0) {
-        return -1; // Indicate error for negative input
-    }
-    long long factorial = 1;
-    // Loop from 1 to n to multiply numbers
-    for (int i = 1; i <= n; ++i) {
-        factorial *= i;
-    }
-    return factorial;
+#include<iostream>
+using namespace std;
+int count = 0;
+struct moodEntry
+{
+	int day;
+	int month;
+	int year;
+	int moodLevel;
+	string moodtype;
+	string note;
+};
+moodEntry moods[100];
+void addMood(moodEntry moods[])
+{
+	cout << "Enter the day\n";
+	cin >> moods->day;
+	cout << "Enter the month\n";
+	cin >> moods->month;
+	cout << "Enter the year\n";
+	cin >> moods->year;
+	cout << "Enter the mood level\n";
+	cin >> moods->moodLevel;
+	do
+	{
+       cout<<"wrong input please "
+	} while (moods->moodLevel > 5 || moods->moodLevel < 1) ;
+	cout << "Enter the mood type \n";
+	cin >> moods->moodtype;
+	cout << "Enter the note\n";
+	cin >> moods->note;
 }
+int main()
+{
 
-int main() {
-    int number;
-
-    std::cout << "Enter a positive integer: ";
-
-    // Take input from the user
-    if (!(std::cin >> number)) {
-        std::cerr << "Invalid input. Please enter an integer." << std::endl;
-        return 1; // Exit with error
-    }
-
-    // Check if the number is positive
-    if (number < 0) {
-        std::cout << "Factorial is not defined for negative numbers." << std::endl;
-    }
-    else {
-        // Call the function to calculate factorial
-        long long result = calculateFactorial(number);
-        if (result == -1) {
-            // This case should ideally not be reached due to the check above
-            std::cerr << "An unexpected error occurred during calculation." << std::endl;
-        }
-        else {
-            // Display the result
-            std::cout << "Factorial of " << number << " is " << result << std::endl;
-        }
-    }
-
-    return 0; // Successful execution
+	int moodCount = 0;
+	 
+	return 0;
 }
